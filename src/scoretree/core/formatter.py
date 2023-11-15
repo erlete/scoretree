241e69b2db89e678ac9753732e5ea62256201566
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-11-15 07:19:46 +0100

    Document code

--- src/scoretree/core/formatter.py
+++ src/scoretree/core/formatter.py
@@ -4,12 +4,29 @@ init()
 
 
 class Formatter:
+    """Formatting class.
+
+    This class contains several methods used for string manipulation and
+    formatting operations, such as indentation or colorization.
+
+    Attributes:
+        START_CHAR (str): character used to indicate start of a new line.
+        MIDDLE_CHAR (str): character used to indicate continuation of a line.
+    """
 
     START_CHAR = "└"
     MIDDLE_CHAR = "─"
 
     @classmethod
-    def indent(cls, count: int = 1):
+    def indent(cls, count: int = 1) -> str:
+        """Generate an indentation string.
+
+        Args:
+            count (int, optional): number of indents. Defaults to 1.
+
+        Returns:
+            str: indented string.
+        """
         count = max(0, count)  # Normalize value.
 
         if count == 1:
@@ -23,7 +40,16 @@ class Formatter:
         )
 
     @classmethod
-    def colorize(cls, string, value):
+    def colorize(cls, string: str, value: int) -> str:
+        """Colorize a string based on a value.
+
+        Args:
+            string (str): string to colorize.
+            value (int): value to base colorization on.
+
+        Returns:
+            str: colorized string.
+        """
         return {
             value < .5: Fore.RED,
             .5 <= value <= .75: Fore.YELLOW,
