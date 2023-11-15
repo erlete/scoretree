0048bef240839a82ef201d184ae4a8a45ef88ab9
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-11-15 07:23:20 +0100

    Add type and value enforcement

--- src/scoretree/core/formatter.py
+++ src/scoretree/core/formatter.py
@@ -24,10 +24,16 @@ class Formatter:
         Args:
             count (int, optional): number of indents. Defaults to 1.
 
+        Raises:
+            TypeError: if count is not an integer.
+
         Returns:
             str: indented string.
         """
-        count = max(0, count)  # Normalize value.
+        if not isinstance(count, int):
+            raise TypeError("count must be an integer.")
+
+        count = max(0, count)  # Value normalization.
 
         if count == 1:
             return (
@@ -40,16 +46,31 @@ class Formatter:
         )
 
     @classmethod
-    def colorize(cls, string: str, value: int) -> str:
-        """Colorize a string based on a value.
+    def colorize(cls, text: str, value: int | float) -> str:
+        """Colorize text based on a value.
 
         Args:
-            string (str): string to colorize.
-            value (int): value to base colorization on.
+            text (str): text to colorize.
+            value (int | float): value to base colorization from 0 to 1 (both
+                included).
+
+        Raises:
+            TypeError: if text is not a string.
+            TypeError: if value is not an integer or float.
+            ValueError: if value is not between 0 and 1.
 
         Returns:
-            str: colorized string.
+            str: colorized text.
         """
+        if not isinstance(string, str):
+            raise TypeError("text must be a string.")
+
+        if not isinstance(value, (int, float)):
+            raise TypeError("value must be an integer or float.")
+
+        if not 0 <= value <= 1:
+            raise ValueError("value must be between 0 and 1.")
+
         return {
             value < .5: Fore.RED,
             .5 <= value <= .75: Fore.YELLOW,
